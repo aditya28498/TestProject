bcb3a6a127e6f5ff9419680cb7b029f1fc35fcf2
aditya <aditya@kloud9-ThinkPad-L450>
2020-01-16 15:17:19 +0530

    First Commit

---
+++ Samplee/src/Sample2/Firstexmp.java
@@ -0,0 +1,11 @@
+package Sample2;
+
+public class Firstexmp {
+
+	public static void main(String[] args) {
+		System.out.println("cpmleted task 1 of docker");
+		System.out.println("completed task 2 of jenkins");
+
+	}
+
+}
